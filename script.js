4c75ec05f64e6cedf0155b68988e1b665802f9ce
Delali Kpessou <delalirody@gmail.com>
2026-03-10 16:22:17 +0000

    I used a button onclick event to toggle the visibility of the navigation links for mobile view. I also added a script to highlight the active page in the navigation menu. Additionally, I made some adjustments to the HTML structure and CSS styles to improve the layout and responsiveness of the website.

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ if (position) {
 }
 });
 
+
+// Dropdown menu functionality
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
